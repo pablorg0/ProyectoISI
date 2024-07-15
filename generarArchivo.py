404704bba3b036605926edb6993ffbd4ad1c254a
maxihm1109 <127493360+maxihm1109@users.noreply.github.com>
2024-07-15 13:54:44 +0200

    Update generarArchivo.py

--- generarArchivo.py
+++ generarArchivo.py
@@ -1,5 +1,4 @@
 import mysql.connector
-import os
 
 config = {
     'host': 'proyectoisiv2-server.postgres.database.azure.com',
@@ -8,33 +7,33 @@ config = {
     'database': 'proyectoisiv2-database'
 }
 
-def write_to_file(file_name, query):
-    try:
-        cnx = mysql.connector.connect(**config)
-        cursor = cnx.cursor()
-        cursor.execute(query)
-        
-        # Obtener la ruta absoluta del archivo
-        file_path = os.path.join(os.getcwd(), file_name)
-        
-        with open(file_path, 'w') as f:
-            for row in cursor:
-                f.write(' \\ '.join(map(str, row)) + '\n')
-        print(f"Archivo '{file_name}' creado exitosamente en '{file_path}'")
+# Inicia la conexión
+cnx = mysql.connector.connect(**config)
+cursor = cnx.cursor()
 
-        cursor.close()
-        cnx.close()
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-    except Exception as e:
-        print(f"Exception: {e}")
-
-# Queries para obtener los datos
+#Para productsWalmart
 queryWalmart = "SELECT * FROM unlocked_phones"
-queryAliexpress = "SELECT * FROM resistant_smartphones"
-queryAPI = "SELECT * FROM electronics"
+cursor.execute(queryWalmart)
 
-# Crear archivos
-write_to_file('productsWalmart.txt', queryWalmart)
-write_to_file('productsAliexpress.txt', queryAliexpress)
-write_to_file('productsAPI.txt', queryAPI)
+with open('productsWalmart.txt', 'w') as f:
+    for (id, name, price, url, img_url) in cursor:
+        f.write(f'{id} \\ {name} \\ {price} \\ {url} \\ {img_url}\n')
+
+#Para productsAliexpress
+queryAliexpress = "SELECT * FROM resistant_smartphones"
+cursor.execute(queryAliexpress)
+
+with open('productsAliexpress.txt', 'w') as f:
+    for (id, name, price, url, img_url) in cursor:
+        f.write(f'{id} \\ {name} \\ {price} \\ {url} \\ {img_url}\n')
+
+#Para FakeStoreAPI
+queryAPI = "SELECT * FROM electronics" 
+cursor.execute(queryAPI)
+
+with open('productsAPI.txt', 'w') as f:
+    for (id, name, price, description, category, image, url) in cursor:
+        f.write(f'{id} \\ {name} \\ {price} \\ {description} \\ {category} \\ {image} \\ {url}\n')
+
+cursor.close()
+cnx.close()
