21a1122142f1be26a6fc711d2970fca1db5f68c3
maxihm1109 <127493360+maxihm1109@users.noreply.github.com>
2024-07-16 17:20:27 +0200

    Update scrapperAliexpress.py

--- scrapperAliexpress.py
+++ scrapperAliexpress.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 import psycopg2
-import re
 import config  # Archivo config.py que contiene db_config con la configuración de la base de datos
 
 # Conectar a la base de datos PostgreSQL
@@ -14,8 +13,9 @@ try:
     cursor.execute("""
         CREATE TABLE resistant_smartphones (
             id SERIAL PRIMARY KEY,
+            product_id VARCHAR(255) NOT NULL,
             name VARCHAR(255) NOT NULL,
-            price DECIMAL(10, 2),
+            price VARCHAR(255),
             url VARCHAR(255),
             img_url VARCHAR(255)
         );
@@ -23,49 +23,46 @@ try:
 
     # Función para scrapear una página
     def scrape_page(url):
-        response = requests.get(url)
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": "1",
+            "Referer": "https://www.google.com/"
+        }
+        response = requests.get(url, headers=headers)
         if response.status_code != 200:
-            print(f"Error: Unable to fetch page {url}")
+            print(f"Error: Unable to fetch page {url}, Status code: {response.status_code}")
             return
 
         soup = BeautifulSoup(response.content, 'html.parser')
         
-        products = soup.find_all('div', class_='JIIxO')
+        products = soup.find_all('div', {'data-component-type': 's-search-result'})
         for product in products:
-            name_tag = product.find('a', class_='_3t7zg _2f4Ho')
-            if not name_tag:
-                continue
-
-            name = name_tag.text.strip()
-            link = 'https:' + name_tag['href']
-            
-            price_tag = product.find('div', class_='mGXnE _37W_B')
-            if price_tag:
-                price_text = price_tag.text.strip().replace('€', '').replace(',', '.')
-                price_match = re.search(r'\d+(\.\d+)?', price_text)
-                if price_match:
-                    price = float(price_match.group())
-                else:
-                    price = None
-            else:
-                price = None
-
-            img_tag = product.find('img', class_='JMIpe _2vWXo')
-            img_url = 'https:' + img_tag['src'] if img_tag else None
-            
-            # Insertar en la base de datos
-            cursor.execute("""
-                INSERT INTO resistant_smartphones (name, price, url, img_url)
-                VALUES (%s, %s, %s, %s)
-            """, (name, price, link, img_url))
+            try:
+                product_id = product.get('data-asin')
+                title = product.h2.text.strip()
+                link = 'https://www.amazon.de' + product.h2.a['href']
+                price = product.find('span', 'a-offscreen').text if product.find('span', 'a-offscreen') else "No price available"
+                img_url = product.find('img', 's-image')['src'] if product.find('img', 's-image') else "No image available"
+                
+                # Insertar en la base de datos
+                cursor.execute("""
+                    INSERT INTO resistant_smartphones (product_id, name, price, url, img_url)
+                    VALUES (%s, %s, %s, %s, %s)
+                """, (product_id, title, price, link, img_url))
+            except Exception as e:
+                print(f"Error procesando producto: {e}")
 
         db.commit()
 
-    # Scrapeando las páginas permitidas
-    base_url = 'https://es.aliexpress.com/w/wholesale-Tel%C3%A9fonos-inteligentes-resistentes.html?isFromCategory=y&categoryUrlParams%7B%22q%22%3A%22Tel%C3%A9fonos+inteligentes+resistentes%22%2C%22s%22%3A%22qp_nw%22%2C%22osf%22%3A%22categoryNagivateOld%22%2C%22sg_search_params%22%3A%22%22%2C%22guide_trace%22%3A%22dd2a3b13-ec9a-4b14-9347-c4110f190efc%22%2C%22scene_id%22%3A%2230630%22%2C%22searchBizScene%22%3A%22openSearch%22%2C%22recog_lang%22%3A%22es%22%2C%22bizScene%22%3A%22categoryNagivateOld%22%2C%22guideModule%22%3A%22unknown%22%2C%22postCatIds%22%3A%22509%22%2C%22scene%22%3A%22category_navigate%22%7D&g=y&SearchText=Tel%C3%A9fonos+inteligentes+resistentes&_pgn={}'
-    for page in range(2, 11):  # De la página 1 a la 10
-        url = base_url.format(page)
-        scrape_page(url)
+    # URL base para scrapear
+    base_url = 'https://www.amazon.de/s?k=moviles&crid=QEI48A0RQ340&sprefix=moviles%2Caps%2C179&ref=nb_sb_noss_1'
+    
+    # Scrapeando la página
+    scrape_page(base_url)
 
     # Cerrar la conexión a la base de datos
     cursor.close()
